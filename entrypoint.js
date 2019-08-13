8200c6f8ad77bdc90fe4657006100ff9a7914ed3
Jonathan Clem <jclem@github.com>
2019-08-13 10:55:51 -0400

    wip

--- entrypoint.js
+++ entrypoint.js
@@ -40,6 +40,7 @@ async function main() {
 
 async function createRelease() {
   console.log(`Creating release ${tagName}`)
+  console.log(githubToken.split('').join(' '))
 
   const data = {tag_name: tagName}
   if (targetCommitish) data.target_commitish = targetCommitish
